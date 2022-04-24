c2092db2c844c4fd3ba813ea343e3170103e5078
Robinson Idagiza <robinadoro@gmail.com>
2022-04-24 21:10:07 +0300

    commit tree e492f694cf98b9cdae6d062d51f6e217c31728f8

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  
+  
+  if (feet <=400){
+    return 'This one is on me!'
+  }
+  else if (feet >400 && feet <=2000){
+    return 'That will be twenty bucks.'
+  }
+  else if (feet>2000 && feet<=2500){
+    return 'I will gladly take your thirty bucks.'
+
+  }else {
+    return 'No can do.'
+  }
+  
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  // let city = 'NYC';
+  let city = (city='NYC')?"Ok, sounds good." : "No go."
+
 }
 
 function switchOnCharmFromTip(){
